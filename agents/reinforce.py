b3f05eb972f92df1326965d1b13d84f945005b09
Artem Kondyukov <artemkondyukov@gmail.com>
2017-01-16 23:02:30 +0300

    One-and-a-half lines of code have been written.

--- agents/reinforce.py
+++ agents/reinforce.py
@@ -3,6 +3,13 @@ import agents.base
 from models import model
 
 
+# Number of past events to store results about
+MEMORY_SIZE = 10 ** 5
+IMG_WIDTH = 84
+IMG_HEIGHT = 84
+LEARNING_SEQ_LEN = 4
+
+
 class Reinforce(agents.base.Agent):
 
     def __init__(self,
@@ -17,8 +24,24 @@ class Reinforce(agents.base.Agent):
         self.batch_size = batch_size
         self.model = model.get_model_deepmind()
 
+        self.state_mem = np.zeros((MEMORY_SIZE, IMG_HEIGHT, IMG_WIDTH))
+        self.action_mem = np.zeros((MEMORY_SIZE, ))
+        self.reward_mem = np.zeros((MEMORY_SIZE, ))
+        self.mem_ptr = 0
+
     def act(self, state, *args, **kwargs):
-        pass
+        return np.argmax(model.predict(state))
 
     def react(self, state, action, reward, done, new_state, *args, **kwargs):
-        pass
+        self.state_mem[self.mem_ptr, :, :] = state
+        self.action_mem[self.mem_ptr] = action
+        self.reward_mem[self.mem_ptr] = reward
+        self.mem_ptr = (self.mem_ptr + 1) % MEMORY_SIZE
+
+        if self.mem_ptr > LEARNING_SEQ_LEN:
+            idx = np.random.randint(LEARNING_SEQ_LEN-1, self.mem_ptr-1, self.batch_size)
+            image_batch = np.zeros((self.batch_size, LEARNING_SEQ_LEN, IMG_HEIGHT, IMG_WIDTH))
+            for i in range(LEARNING_SEQ_LEN):
+                image_batch[:, i, :, :] = self.state_mem[idx-i]
+            
+            pass
