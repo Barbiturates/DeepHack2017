57bf7e2c5abdb61110c08e2d7dd8e87c9bbb12b0
Emil Magerramov <emilmagerramov@Emils-MacBook-Pro.local>
2017-01-16 17:57:41 +0300

    Added deepmind model

---
+++ models/model.py
@@ -0,0 +1,29 @@
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Convolution2D, Dense, Input
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from keras import backend as K
+
+DIM = 84
+NUM_ACTIONS = 3
+
+
+def get_model_deepmind(dim=DIM, num_actions=NUM_ACTIONS):
+    main_input = Input(shape=(dim, dim, 4))
+    input_shape = (-1, 4, dim, dim)
+
+    x = Convolution2D(16, 8, 8, input_shape=input_shape, activation='relu',
+                      subsample=(4, 4), border_mode='same', dim_ordering='tf')(main_input)
+
+    x = Convolution2D(16, 8, 8, border_mode='same', activation='relu',
+                      subsample=(4, 4), dim_ordering='tf')(x)
+
+    x = Convolution2D(32, 4, 4, border_mode='same', activation='relu',
+                      subsample=(2, 2), dim_ordering='tf')(x)
+    x = Flatten()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dense(num_actions, activation='sigmoid')(x)
+
+    model = Model(input=main_input, output=x)
+    model.compile(optimizer='adam', loss='mse')
+    return model
