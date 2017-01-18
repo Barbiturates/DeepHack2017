841fae010c7bbb3aec6195c4077695253b297b7b
Vlad <vladt9@yandex.ru>
2017-01-19 01:23:35 +0300

    Fixed network model

--- models/model.py
+++ models/model.py
@@ -11,19 +11,20 @@ LEARNING_SEQ_LEN = 4
 
 def get_model_deepmind(dim=DIM, num_actions=NUM_ACTIONS):
     main_input = Input(shape=(dim, dim, LEARNING_SEQ_LEN))
-    input_shape = (-1, LEARNING_SEQ_LEN, dim, dim)
+    # input_shape = (-1, LEARNING_SEQ_LEN, dim, dim)
+    input_shape = (dim, dim, LEARNING_SEQ_LEN)
 
     x = Convolution2D(16, 8, 8, input_shape=input_shape, activation='relu',
                       subsample=(4, 4), border_mode='same', dim_ordering='tf')(main_input)
 
-    x = Convolution2D(16, 8, 8, border_mode='same', activation='relu',
-                      subsample=(4, 4), dim_ordering='tf')(x)
+    # x = Convolution2D(16, 8, 8, border_mode='same', activation='relu',
+    #                   subsample=(4, 4), dim_ordering='tf')(x)
 
     x = Convolution2D(32, 4, 4, border_mode='same', activation='relu',
                       subsample=(2, 2), dim_ordering='tf')(x)
     x = Flatten()(x)
     x = Dense(256, activation='relu')(x)
-    x = Dense(num_actions, activation='sigmoid')(x)
+    x = Dense(num_actions)(x)
 
     model = Model(input=main_input, output=x)
     model.compile(optimizer='adam', loss='mse')
